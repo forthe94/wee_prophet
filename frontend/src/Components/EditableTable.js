549aa7b26bd1ff860d2fbcfb0ed52f42707cd730
Tetenkov Alexander <forthe@ya.ru>
2023-03-05 15:12:23 +0400

    reload data from db works

--- frontend/src/Components/EditableTable.js
+++ frontend/src/Components/EditableTable.js
@@ -6,12 +6,13 @@ import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 import axios from "axios";
 import config from "../config.json";
-const weekday = ["Вс","Пн","Вт","Ср","Чт","Пт","Сб"];
+
+const weekday = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
 let initialRows = [
   {id: 0, deed_name: 'Настроение'},
 ];
 
-function getDateWithDaysDiff(date, days, wd=false) {
+function getDateWithDaysDiff(date, days, wd = false) {
   date.setHours(0, 0, 0, 0)
   date = new Date(date.getTime() + days * 86400000)
   let ret = date.getFullYear() + '/'
@@ -22,6 +23,20 @@ function getDateWithDaysDiff(date, days, wd=false) {
 
 }
 
+const getNextId = (rows) => {
+  let cur_id = 0;
+  while (true) {
+    let found = false;
+    for (let ind = 0; ind < rows.length; ind++) {
+      if (cur_id === rows[ind]['id']) {
+        found = true;
+        break
+      }
+    }
+    if (found === false) return cur_id
+    cur_id++
+  }
+}
 let date_keys = []
 let date_headers = []
 let nowDate = new Date();
@@ -33,73 +48,88 @@ for (let day = -5; day < 5; day++) {
   initialRows[0][date_key] = '0'
 }
 
-function getDeedsFromBackend() {
-  const token = localStorage.getItem('token')
-  axios.post(
-    config.SERVER_URL + '/get-deed-records',
-    {
-      'dates': date_keys
-    },
-    {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      },
-    })
-    .then((response) => {
-      console.log(response)
-    })
-    .catch((error) => console.log(error));
-}
-
 export default function DataGridDemo() {
   const [rows, setRows] = React.useState(initialRows);
   const rowsState = useRef();
+
+  function getDeedsFromBackend() {
+    const token = localStorage.getItem('token')
+    console.log("in useEffect")
+    axios.post(
+      config.SERVER_URL + '/get-deed-records',
+      {
+        'dates': date_keys
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setRows((oldRows) => {
+          console.log('set_rows', rows, response)
+          let tmpRows = [...oldRows]
+          let deeds_list = response['data']['deeds_list']
+          for (let i = 0; i < deeds_list.length; i++) {
+            if (i !== 0) {
+              tmpRows.push({'deed_name': deeds_list[i], id: i})
+            }
+            for (let key_ind = 0; key_ind < date_keys.length; key_ind++){
+              tmpRows[i][date_keys[key_ind]] = response['data'][date_keys[key_ind]]['deeds'][deeds_list[i]]
+            }
+          }
+          return tmpRows
+        })
+      })
+      .catch((error) => console.log(error));
+  }
+
   useEffect(getDeedsFromBackend, []);
+
   // Из rowsState можно брать текущее состояние таблицы
   rowsState.current = rows
-  const postToBackend = () => {
-    let currentRows = rowsState.current
-      let deeds = {}
-      for (let cur_deed_name_ind = 0; cur_deed_name_ind < currentRows.length; cur_deed_name_ind++)
-      {
-        let deed_name = currentRows[cur_deed_name_ind]['deed_name']
-        let vals = []
-        for (let date_num = 0; date_num < date_keys.length; date_num++)
-        {
-          vals.push(currentRows[cur_deed_name_ind][date_keys[date_num]])
-        }
-        deeds[deed_name] = vals
+  const postToBackend = (currentRows) => {
+    let deeds = {}
+    for (let cur_deed_name_ind = 0; cur_deed_name_ind < currentRows.length; cur_deed_name_ind++) {
+      let deed_name = currentRows[cur_deed_name_ind]['deed_name']
+      let vals = []
+      for (let date_num = 0; date_num < date_keys.length; date_num++) {
+        vals.push(currentRows[cur_deed_name_ind][date_keys[date_num]])
       }
-      const token = localStorage.getItem('token')
+      deeds[deed_name] = vals
+    }
+    const token = localStorage.getItem('token')
 
-      axios.post(
-        config.SERVER_URL + '/save-deed-records',
-        {
-          'dates': date_keys,
-          'deeds': deeds
+    axios.post(
+      config.SERVER_URL + '/save-deed-records',
+      {
+        'dates': date_keys,
+        'deeds': deeds
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`,
         },
-        {
-          headers: {
-            'Authorization': `Bearer ${token}`,
-          },
-        })
-        .then((response) => {
-          console.log(response)
-        })
-        .catch((error) => console.log(error));
+      })
+      .then((response) => {
+
+      })
+      .catch((error) => console.log(error));
 
   }
   const onDeleteButtonClick = (e, id) => {
     e.stopPropagation();
-    postToBackend()
     setRows(rows.filter((initialRows) => initialRows.id !== id));
+    let currentRows = rowsState.current
+    postToBackend(rows.filter((initialRows) => initialRows.id !== id))
+
   };
   let columns = [
     {field: 'id', headerName: 'ID', hide: true},
     {field: 'deed_name', headerName: '', width: 250, editable: true},
 
   ]
-  for (let day=0; day < 10; day++) {
+  for (let day = 0; day < 10; day++) {
     columns.push({
       field: date_keys[day],
       headerName: date_headers[day],
@@ -124,31 +154,19 @@ export default function DataGridDemo() {
   );
 
   const handleClick = () => {
-    const getNextId = () => {
-      let cur_id = 0;
-      while (true) {
-        let found = false;
-        for (let ind = 0; ind < rows.length; ind++) {
-          if (cur_id === rows[ind]['id']) {
-            found = true;
-            break
-          }
-        }
-        if (found === false) return cur_id
-        cur_id++
-      }
-    }
-    postToBackend()
-    let newRow = {id: getNextId(), deed_name: 'Название дела'}
-    for (let day = 0; day < 10; day++){
+
+    let newRow = {id: getNextId(rows), deed_name: 'Название дела'}
+    for (let day = 0; day < 10; day++) {
       newRow[date_keys[day]] = '0'
     }
 
     setRows((oldRows) => [...oldRows, newRow]);
+    let currentRows = rowsState.current
+
+    postToBackend([...currentRows, newRow])
   };
   const processRowUpdate = React.useCallback(
     async (newRow) => {
-      postToBackend()
 
       setRows((oldRows) => {
         let rowInd = 0
@@ -157,6 +175,7 @@ export default function DataGridDemo() {
         }
         let tmpRows = [...oldRows]
         tmpRows[rowInd] = newRow
+        postToBackend(tmpRows)
         return tmpRows
       })
       return newRow
